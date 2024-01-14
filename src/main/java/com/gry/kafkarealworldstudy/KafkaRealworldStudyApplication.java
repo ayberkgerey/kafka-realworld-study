4b8d1d0188caa60994faf17a309e5f0c5639a2f8
Ayberk Gerey <ayberkgerey@gmail.com>
2024-01-14 17:26:49 +0300

    Initial commit

---
+++ src/main/java/com/gry/kafkarealworldstudy/KafkaRealworldStudyApplication.java
@@ -0,0 +1,13 @@
+package com.gry.kafkarealworldstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRealworldStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaRealworldStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gry/kafkarealworldstudy/KafkaRealworldStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gry.kafkarealworldstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRealworldStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
